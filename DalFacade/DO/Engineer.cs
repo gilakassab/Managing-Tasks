756586ddfafc2a0b3bfc68be43c0bfeee0698fb0
ShiraBraverman <shira7656@gmail.com>
2023-10-19 22:03:48 +0300

    Stage 1 part 3 (Engineer)

---
+++ DalFacade/DO/Engineer.cs
@@ -0,0 +1,13 @@
+﻿namespace DO;
+
+/// <summary>
+/// 
+/// </summary>
+class record Engineer
+{
+    string Id,
+    string Name,
+    string? Email,
+    EngineerExperience level,
+    double? cost
+}
