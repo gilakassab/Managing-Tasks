b2f6186128b38a61ccad1c031a43ec4565de6400
gilakassab <gilakassab@gmail.com>
2023-11-06 15:36:20 +0200

    stage 1 part 9

--- DalTest/Initialization.cs
+++ DalTest/Initialization.cs
@@ -9,7 +9,7 @@ public static class Initialization
     private static IEngineer? s_dalEngineer; //stage 1
     private static ITask? s_dalTask; //stage 1
     private static readonly Random s_rand = new();
-    public static void Do()
+    public static void Do(IDependency? s_dalDependency, IEngineer? s_dalEngineer, ITask? s_dalTask)
     {
         IDependency? dalDependency=null;
         IEngineer? dalEngineer=null;
