e803e5eed4ded402f06a660a1db01a87fa8acb52
gilakassab <gilakassab@gmail.com>
2023-11-27 14:42:39 +0200

    g

---
+++ DalFacade/DalApi/ICrud.cs
@@ -0,0 +1,18 @@
+﻿using DO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalApi
+{
+    public interface ICrud<T> where T : class
+    {
+        int Create<T>(); //Creates new entity object in DAL
+        Dependency? Read<T>(); //Reads entity object by its ID 
+        List<Dependency> ReadAll(); //stage 1 only, Reads all entity objects
+        void Update(< item); //Updates entity object
+        void Delete(int id); //Deletes an object by its Id
+    }
+}
