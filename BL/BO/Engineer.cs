111d31bc1e2a8d1dfddbc8e52cc9c11ec7061b5b
ShiraBraverman <shira7656@gmail.com>
2023-12-19 22:37:34 +0200

    Stage 4 part 6 and its longgggg

--- BL/BO/Engineer.cs
+++ BL/BO/Engineer.cs
@@ -1,22 +1,16 @@
-﻿using DO;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.CompilerServices;
+namespace BO;
 
-namespace BO
+public class Engineer
 {
-    public class Engineer
-    {
-        public int Id { get; init; }
-        public string? Name { get; set; }
-        public bool IsActive { get; set; }
-        public string Email { get; set; }
-        public EngineerExperience Level { get; set; }
-        public double Cost { get; set; }
-   
-        public override string ToString() => this.ToStringProperty();
+    public int Id { get; init; }
+    public string? Name { get; set; }
+    public bool IsActive { get; set; }
+    public string Email { get; set; }
+    public EngineerExperience Level { get; init; }
+    public double Cost { get; set; }
+    public int CurrentTaskId { get; set; }
+    public string CurrentTaskDescription { get; set; }
 
-    }
+    public override string ToString() => this.ToStringProperty();
 }
--- BL/BO/Enums.cs
+++ BL/BO/Enums.cs
@@ -1,4 +1,5 @@
-﻿
-namespace BO;
+﻿namespace BO;
 
+public enum EngineerExperience { Beginner, AdvancedBeginner, Intermediate, Advanced, Expert }
 
+public enum Status { Unscheduled, Scheduled, OnTrack, InJeopardy }
