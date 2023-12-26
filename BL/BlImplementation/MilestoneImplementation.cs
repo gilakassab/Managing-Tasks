a9d4fde22778da34e5b5035895658b093a062e2b
gilakassab <gilakassab@gmail.com>
2023-12-26 15:55:42 +0200

    stage4 part 8

--- BL/BlImplementation/MilestoneImplementation.cs
+++ BL/BlImplementation/MilestoneImplementation.cs
@@ -1,6 +1,7 @@
 ﻿
 using BlApi;
 using BO;
+using DO;
 
 namespace BlImplementation;
 
@@ -19,6 +20,18 @@ internal class MilestoneImplementation : IMilestone
 
     public void Update(Milestone item)
     {
-        throw new NotImplementedException();
+        DO.Task doTask = _dal.Task.Read(e => e.Id == boTask.Id);
+        if (doEngineer is null)
+            throw new DalDoesNotExistException($"Engineer with ID={boEngineer.Id} does not exist");
+
+
+        try
+        {
+            _dal.Engineer.Update(doEngineer);
+        }
+        catch (DO.DalAlreadyExistsException ex)
+        {
+            throw new BO.BlAlreadyExistsException($"Engineer with ID={boEngineer.Id} not exists", ex);
+        }
     }
 }
