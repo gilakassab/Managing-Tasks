8b8c58a94f4211e31982b721c3730de6a704a8cb
ShiraBraverman <shira7656@gmail.com>
2024-01-30 20:59:11 +0200

    Stage 6 second commit

--- PL/Engineer/EngineerWindow.xaml.cs
+++ PL/Engineer/EngineerWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using BO;
+using PL.Task;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,8 +22,35 @@ namespace PL.Engineer
     /// </summary>
     public partial class EngineerWindow : Window
     {
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+
+        // מאפיינים של המהנדס
+        public BO.Engineer Engineer
+        {
+            get { return (BO.Engineer)GetValue(EngineerProperty); }
+            set { SetValue(EngineerProperty, value); }
+        }
+
+        public static readonly DependencyProperty EngineerProperty =
+            DependencyProperty.Register("Engineer", typeof(BO.Engineer), typeof(EngineerWindow), new PropertyMetadata(null));
+
+        public BO.EngineerExperience EngExperience { get; set; }
+        public BO.Roles Role { get; set; }
+
+        // בנאי החלון
         public EngineerWindow(int id = 0)
         {
+            try
+            {
+                if (id != 0)
+                    Engineer = s_bl!.Engineer.Read(id)!;
+                else
+                    Engineer = new BO.Engineer();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"{ex}");
+            }
             InitializeComponent();
         }
     }
